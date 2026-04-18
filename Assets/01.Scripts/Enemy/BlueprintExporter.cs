14396e2f318baedb2b358ed8c169299044351f9f
owl3927 <heich4088@gmail.com>
2026-04-18 16:31:53 +0900

    Enemy Create Prefab Editor

---
+++ Assets/01.Scripts/Enemy/BlueprintExporter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueprintExporter : MonoBehaviour
+{
+    [SerializeField] private GridBoard board;
+    public string exportFileName = "NewEnemyBlueprint";
+
+    public List<PartPlacementData> ExportCurrentBoard()
+    {
+        HashSet<PlacedPart> allParts = board.GetAllParts();
+        List<PartPlacementData> result = new();
+
+        foreach (var part in allParts)
+        {
+            if (part == null || part.data == null)
+                continue;
+
+            PartPlacementData placement = new PartPlacementData
+            {
+                partKey = part.data.Key,
+                origin = part.origin,
+                rotation = part.rotation
+            };
+
+            result.Add(placement);
+        }
+
+        return result;
+    }
+}
---
+++ Assets/01.Scripts/Enemy/EnemyBuilder.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class EnemyBuilder : MonoBehaviour
+{
+
+    [SerializeField] private GridBoard board;
+    [SerializeField] private GridRenderer gridRenderer;
+    [SerializeField] private Transform placedPartsRoot;
+    public EnemyBlueprintSO blueprint;
+
+    private void Start()
+    {
+        BuildFromBlueprint();
+    }
+
+    public void BuildFromBlueprint()
+    {
+        if (blueprint == null) return;
+
+        foreach (var placement in blueprint.placements)
+        {
+            if (!GridManager.instance.partDic.TryGetValue(placement.partKey, out PartData partData))
+            {
+                Debug.LogWarning($"Dont find part key");
+                continue;
+            }
+
+            GameObject partObj = new GameObject($"EnemyPart_{partData.PartName}");
+            partObj.transform.SetParent(placedPartsRoot);
+
+            PlacedPart placedPart = partObj.AddComponent<PlacedPart>();
+
+            bool success = board.PlacePart(partData, placement.origin, placement.rotation, placedPart);
+
+            if (!success)
+            {
+                Debug.LogWarning($"배치 실패: {partData.PartName}, {placement.origin}");
+                Destroy(partObj);
+                continue;
+            }
+
+            placedPart.BuildVisual(gridRenderer, placedPart.transform, Color.white);
+        }
+    }
+}
