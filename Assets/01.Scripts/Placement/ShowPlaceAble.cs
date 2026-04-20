878bc894056886749e5cfae4fa7e0d665b547caf
zerobean <78115363+zerobean0816@users.noreply.github.com>
2026-04-21 06:03:07 +0900

    UI 업데이트

--- Assets/01.Scripts/Placement/ShowPlaceAble.cs
+++ Assets/01.Scripts/Placement/ShowPlaceAble.cs
@@ -6,12 +6,10 @@ public class ShowPlaceAble : MonoBehaviour
     [SerializeField] TextMeshProUGUI _UIText;
 
 
-    public void ReFreshPlaceableUI()
+    public void ReFreshPlaceableUI(int current, int max)
     {
-        int _currentBlockCount = 0;
-        int _maxBlockCount = 50;
-
-
-        _UIText.text = $"{_currentBlockCount} / {_maxBlockCount}";
+        if (_UIText == null) return;
+        _UIText.text = $"{Mathf.Clamp(current, 0, max)} / {max}";
+         _UIText.color = current >= max ? Color.red : Color.white;
     }
 }
