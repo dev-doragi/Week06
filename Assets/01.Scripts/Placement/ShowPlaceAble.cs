490d496e8967cfa1d1b69b55a52e43c2e01d71f0
zerobean <78115363+zerobean0816@users.noreply.github.com>
2026-04-21 06:12:58 +0900

    블록 설치 확인 UI 추가

--- Assets/01.Scripts/Placement/ShowPlaceAble.cs
+++ Assets/01.Scripts/Placement/ShowPlaceAble.cs
@@ -9,7 +9,7 @@ public class ShowPlaceAble : MonoBehaviour
     public void ReFreshPlaceableUI(int current, int max)
     {
         if (_UIText == null) return;
-        _UIText.text = $"{Mathf.Clamp(current, 0, max)} / {max}";
+        _UIText.text = $"설치 가능한 블록의 수 : {Mathf.Clamp(current, 0, max)} / {max}";
          _UIText.color = current >= max ? Color.red : Color.white;
     }
 }
